021c27f31be947e6f4862a2198aabcaddc770ad6
Kawthaar-tj <tanjikawthar@hotmail.com>
2026-03-03 13:49:49 +0100

    Update config.example.h

--- firmware/esp32/config.example.h
+++ firmware/esp32/config.example.h
@@ -1,1 +1,15 @@
+#pragma once
 
+// ---- WiFi ----
+#define WIFI_SSID "CHANGE_ME"
+#define WIFI_PASS "CHANGE_ME"
+
+// ---- MQTT ----
+#define MQTT_HOST "192.168.1.10"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "camion/donnees"
+#define DEVICE_ID  "camion_X"
+
+// Optional: if your broker requires login
+#define MQTT_USER ""
+#define MQTT_PASS ""
